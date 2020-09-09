871b0f81ddc1122ba569720cd95f09b5c3304ae9
Nagato Kadoya <nagasushi0519@gmail.com>
2020-09-09 10:43:56 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 角谷長門 on 9/9/20.
+//  Copyright © 2020 Nagato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
